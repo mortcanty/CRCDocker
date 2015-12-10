7569a957ab0fee6bf7778dc99757d555d57ad1c7
mortcanty <mort.canty@gmail.com>
2015-12-10 20:49:45 +0100

    Modified home directory from crc to home

--- src/dispms.py
+++ src/dispms.py
@@ -4,9 +4,9 @@
 #  Purpose:  Display a multispectral image
 #             allowed formats: uint8, uint16,float32,float64 
 #  Usage (from command line):             
-#    python dispms.py [-f filename, -d spatialDimensions -p RGB band positions -e enhancement method]
+#    python dispms.py  
 #
-#  Copyright (c) 2011, Mort Canty
+#  Copyright (c) 2015, Mort Canty
 #    This program is free software; you can redistribute it and/or modify
 #    it under the terms of the GNU General Public License as published by
 #    the Free Software Foundation; either version 2 of the License, or
@@ -17,42 +17,94 @@
 #    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
 #    GNU General Public License for more details.
 
-import sys, getopt, gdal
+import sys, getopt, os
+from osgeo import gdal
 import matplotlib.pyplot as plt
 import  auxil.auxil as auxil 
 import numpy as np
 from osgeo.gdalconst import GA_ReadOnly
 
 def make_image(redband,greenband,blueband,rows,cols,enhance):
-    if str(redband.dtype) == 'uint8':
-        dt = 1
-    elif str(redband.dtype) == 'uint16':
-        dt = 2
-    elif str(redband.dtype) == 'int16':
-        dt = 2        
-    elif str(redband.dtype) == 'float32':
-        dt = 4
-    elif str(redband.dtype) == 'float64':
-        dt = 6
-    else:
-        print 'Unrecognized format'
-        return   
-    redband = redband.tostring()
-    greenband = greenband.tostring()
-    blueband = blueband.tostring()
+    X = np.zeros((rows*cols,3),dtype=np.uint8)  
     if enhance == 'linear255':
-        rng = [0.0,255.0]
-    else:
-        rng = None
-    if dt != 1: 
-        redband   = auxil.byte_stretch(redband,dtype=dt,rng=rng)
-        greenband = auxil.byte_stretch(greenband,dtype=dt,rng=rng)
-        blueband  = auxil.byte_stretch(blueband,dtype=dt,rng=rng)        
-    r,g,b = auxil.stretch(redband,greenband,blueband,enhance)  
-    X = np.zeros((rows*cols,3),dtype=np.float32)                                                                                                                 
-    X[:,0] = np.float32(np.fromstring(r,dtype=np.uint8))
-    X[:,1] = np.float32(np.fromstring(g,dtype=np.uint8))
-    X[:,2] = np.float32(np.fromstring(b,dtype=np.uint8))
+        i = 0
+        for tmp in [redband,greenband,blueband]:
+            tmp = tmp.ravel()
+            tmp = np.where(tmp<0,0,tmp)  
+            tmp = np.where(tmp>255,255,tmp)
+            X[:,i] = np.byte(tmp)
+            i += 1
+    elif enhance == 'linear':
+        i = 0
+        for tmp in [redband,greenband,blueband]:             
+            tmp = tmp.ravel()  
+            mx = np.max(tmp)
+            mn = np.min(tmp)  
+            tmp = (tmp-mn)*255.0/(mx-mn)
+            tmp = np.where(tmp<0,0,tmp)  
+            tmp = np.where(tmp>255,255,tmp)
+            X[:,i] = np.byte(tmp)
+            i += 1
+    elif enhance == 'linear2pc':
+        i = 0
+        for tmp in [redband,greenband,blueband]:     
+            tmp = tmp.ravel()        
+            mx = np.max(tmp)
+            mn = np.min(tmp)  
+            tmp = (tmp-mn)*255.0/(mx-mn)
+            tmp = np.where(tmp<0,0,tmp)  
+            tmp = np.where(tmp>255,255,tmp)
+            hist,bin_edges = np.histogram(tmp,256,(0,256))
+            cdf = hist.cumsum()
+            lower = 0
+            j = 0
+            while cdf[j] < 0.02*cdf[-1]:
+                lower += 1
+                j += 1
+            upper = 255    
+            j = 255
+            while cdf[j] > 0.98*cdf[-1]:
+                upper -= 1
+                j -= 1
+            if upper==0:
+                upper = 255
+                print 'Saturated stretch failed'
+            fp = (bin_edges-lower)*255/(upper-lower) 
+            fp = np.where(bin_edges<=lower,0,fp)
+            fp = np.where(bin_edges>=upper,255,fp)
+            X[:,i] = np.byte(np.interp(tmp,bin_edges,fp))
+            i += 1       
+    elif enhance == 'equalization':   
+        i = 0
+        for tmp in [redband,greenband,blueband]:     
+            tmp = tmp.ravel()    
+            mx = np.max(tmp)
+            mn = np.min(tmp)  
+            tmp = (tmp-mn)*255.0/(mx-mn)
+            tmp = np.where(tmp<0,0,tmp)  
+            tmp = np.where(tmp>255,255,tmp)  
+            hist,bin_edges = np.histogram(tmp,256,(0,256)) 
+            cdf = hist.cumsum()
+            lut = 255*cdf/float(cdf[-1]) 
+            X[:,i] = np.byte(np.interp(tmp,bin_edges[:-1],lut))
+            i += 1
+    elif enhance == 'logarithmic':   
+        i = 0
+        for tmp in [redband,greenband,blueband]:     
+            tmp = tmp.ravel() 
+            mn = np.min(tmp)
+            if mn < 0:
+                tmp = tmp - mn
+            idx = np.where(tmp == 0)
+            tmp[idx] = np.mean(tmp)
+            tmp = np.log(tmp) 
+            mn =np.min(tmp)
+            mx = np.max(tmp)
+            tmp =(tmp-mn)*255.0/(mx-mn)  
+            tmp = np.where(tmp<0,0,tmp)  
+            tmp = np.where(tmp>255,255,tmp)
+            X[:,i] = np.byte(tmp) 
+            i += 1                           
     return np.reshape(X,(rows,cols,3))/255.
 
 def dispms(filename1=None,filename2=None,dims=None,DIMS=None,rgb=None,RGB=None,enhance=None,ENHANCE=None,cls=False,CLS=False):
@@ -87,20 +139,22 @@ def dispms(filename1=None,filename2=None,dims=None,DIMS=None,rgb=None,RGB=None,e
     b = int(np.min([b,bands1]))
     
     if enhance == None:
-        enhance = 2
+        enhance = 5
     if enhance == 1:
-        enhance = 'linear255'
+        enhance1 = 'linear255'
     elif enhance == 2:
-        enhance = 'linear'
+        enhance1 = 'linear'
     elif enhance == 3:
-        enhance = 'linear2pc'
+        enhance1 = 'linear2pc'
     elif enhance == 4:
-        enhance = 'equalization'
+        enhance1 = 'equalization'
+    elif enhance == 5:
+        enhance1 = 'logarithmic'    
     else:
         enhance = 'linear2pc' 
     try:  
         if not cls:
-            redband   = np.nan_to_num(inDataset1.GetRasterBand(r).ReadAsArray(x0,y0,cols,rows))
+            redband   = np.nan_to_num(inDataset1.GetRasterBand(r).ReadAsArray(x0,y0,cols,rows)) 
             greenband = np.nan_to_num(inDataset1.GetRasterBand(g).ReadAsArray(x0,y0,cols,rows)) 
             blueband  = np.nan_to_num(inDataset1.GetRasterBand(b).ReadAsArray(x0,y0,cols,rows))
         else:
@@ -112,17 +166,17 @@ def dispms(filename1=None,filename2=None,dims=None,DIMS=None,rgb=None,RGB=None,e
             blueband = classimg*0
             for k in classes:
                 idx = np.where(classimg==k)
-                redband[idx] = ctable[k-1,0]
-                greenband[idx] = ctable[k-1,1]
-                blueband[idx] = ctable[k-1,2]
+                redband[idx] = ctable[k,0]
+                greenband[idx] = ctable[k,1]
+                blueband[idx] = ctable[k,2]
             redband = np.reshape(redband,(rows,cols))    
             greenband = np.reshape(greenband,(rows,cols))
             blueband = np.reshape(blueband,(rows,cols))
-        inDataset = None   
+        inDataset1 = None   
     except  Exception as e:
         print 'Error in dispms: %s'%e  
         return
-    X1 = make_image(redband,greenband,blueband,rows,cols,enhance)
+    X1 = make_image(redband,greenband,blueband,rows,cols,enhance1)
     if filename2 is not None:
         if DIMS == None:
             DIMS = [0,0,cols2,rows2]
@@ -135,17 +189,19 @@ def dispms(filename1=None,filename2=None,dims=None,DIMS=None,rgb=None,RGB=None,e
         b = int(np.min([b,bands2]))
         enhance = ENHANCE
         if enhance == None:
-            enhance = 2
+            enhance = 5
         if enhance == 1:
-            enhance = 'linear255'
+            enhance2 = 'linear255'
         elif enhance == 2:
-            enhance = 'linear'
+            enhance2= 'linear'
         elif enhance == 3:
-            enhance = 'linear2pc'
+            enhance2 = 'linear2pc'
         elif enhance == 4:
-            enhance = 'equalization'
+            enhance2 = 'equalization'
+        elif enhance == 5:
+            enhance2 = 'logarithmic'    
         else:
-            enhance = 'linear2pc'          
+            enhance = 'logarithmic'          
         try:  
             if not CLS:
                 redband   = np.nan_to_num(inDataset2.GetRasterBand(r).ReadAsArray(x0,y0,cols,rows))
@@ -160,36 +216,36 @@ def dispms(filename1=None,filename2=None,dims=None,DIMS=None,rgb=None,RGB=None,e
                 blueband = classimg*0
                 for k in classes:
                     idx = np.where(classimg==k)
-                    redband[idx] = ctable[k-1,0]
-                    greenband[idx] = ctable[k-1,1]
-                    blueband[idx] = ctable[k-1,2]
+                    redband[idx] = ctable[k,0]
+                    greenband[idx] = ctable[k,1]
+                    blueband[idx] = ctable[k,2]
                 redband = np.reshape(redband,(rows,cols))    
                 greenband = np.reshape(greenband,(rows,cols))
                 blueband = np.reshape(blueband,(rows,cols))
-            inDataset = None   
+            inDataset2 = None   
         except  Exception as e:
             print 'Error in dispms: %s'%e  
             return
-        X2 = make_image(redband,greenband,blueband,rows,cols,enhance)    
+        X2 = make_image(redband,greenband,blueband,rows,cols,enhance2)    
         f, ax = plt.subplots(1,2,figsize=(20,10))
         ax[0].imshow(X1)
-        ax[0].set_title(filename1)
+        ax[0].set_title('%s: %s: %s:  %s\n'%(os.path.basename(filename1),enhance1, str(rgb), str(dims)))
         ax[1].imshow(X2)
-        ax[1].set_title(filename2)
+        ax[1].set_title('%s: %s: %s: %s\n'%(os.path.basename(filename2),enhance2, str(RGB), str(DIMS)))
     else:
-        f, ax = plt.subplots( figsize=(10,10))
+        f, ax = plt.subplots(figsize=(10,10))
         ax.imshow(X1)
-        ax.set_title(filename1) 
+        ax.set_title('%s: %s: %s: %s\n'%(os.path.basename(filename1),enhance1, str(rgb), str(dims))) 
     plt.show()
                       
 
 def main():
     usage = '''Usage: python %s [-c] [-C] [-f filename1] [-F filename2] [-p posf] [P posF [-d dimsf] [-D dimsF]\n
-                                        [-e enhancementf] [-E enhancementF\n
+                                        [-e enhancementf] [-E enhancement]\n
             if -f is not specified it will be queried\n
             use -c or -C for classification image\n 
             RGB bandPositions and spatialDimensions are lists, e.g., -p [1,4,3] -d [0,0,400,400] \n
-            enhancements: 1=linear255 2=linear 3=linear2pc 4=equalization\n'''%sys.argv[0]
+            enhancements: 1=linear255 2=linear 3=linear2pc 4=equalization 5=logarithmic\n'''%sys.argv[0]
     options,args = getopt.getopt(sys.argv[1:],'hcCf:F:p:P:d:D:e:E:')
     filename1 = None
     filename2 = None
